9b2133b016cd6bdf696c31f11de5837e55ff9c97
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-15 13:38:07 -0300

    Se renderiza el componente con su props definida en App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Saludador from './ReactFC'
 
 function App() {
 
   return (
+    // Se retorna el componente con la props definida
     <>
-     
+      <Saludador name="Víctor"/>
     </>
   )
 }
